b8ac13059f808dbfc5b6664bfbdc2dc51a8df3a7
anjali <muskaananjaliagarwal@gmail.com>
2022-07-30 21:40:37 +0530

    metrostation

---
+++ Metro/src/main/java/com/metro/bean/MetroStation.java
@@ -0,0 +1,15 @@
+package com.metro.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+//change Metro to MetroStation
+public class MetroStation {
+	private int id;
+	private String name;
+}
