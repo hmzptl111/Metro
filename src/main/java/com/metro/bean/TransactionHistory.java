dcc1c7e352b919072ab88cf8c0559d1ecb3a5bbb
anjali <muskaananjaliagarwal@gmail.com>
2022-08-26 01:24:03 +0530

    first page css

--- src/main/java/com/metro/bean/TransactionHistory.java
+++ src/main/java/com/metro/bean/TransactionHistory.java
@@ -1,7 +1,6 @@
 package com.metro.bean;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/metro/controller/MetroController.java
+++ src/main/java/com/metro/controller/MetroController.java
@@ -1,28 +1,18 @@
 package com.metro.controller;
 
 import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.metro.bean.Card;
-import com.metro.bean.MetroStation;
 import com.metro.bean.Transaction;
-import com.metro.model.service.CardService;
-import com.metro.model.service.MetroStationService;
-import com.metro.model.service.SignInService;
 import com.metro.model.service.TransactionService;
-import com.metro.model.service.UserService;
 
 @Controller
 public class MetroController {
